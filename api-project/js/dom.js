7924df2e9ee92a53c03b5a433665e0f4e7ad5065
eriks61 <eriks61@nycstudents.net>
2023-12-18 09:22:17 -0500

    hnlk

--- api-project/js/dom.js
+++ api-project/js/dom.js
@@ -1,48 +1,47 @@
 const DOMSelectors = {
-	grass: document.getElementById("btn1"),
-	fire: document.getElementById("btn2"),
-	water: document.getElementById("btn3"),
-	ground: document.getElementById("btn4"),
-	rock: document.getElementById("btn5"),
-	ghost: document.getElementById("btn6"),
-	gallery: document.getElementById("card-container"),
-	themeSwitch: document.getElementById("btn7"),
+  grass: document.getElementById("btn1"),
+  fire: document.getElementById("btn2"),
+  water: document.getElementById("btn3"),
+  ground: document.getElementById("btn4"),
+  rock: document.getElementById("btn5"),
+  ghost: document.getElementById("btn6"),
+  gallery: document.getElementById("card-container"),
+  themeSwitch: document.getElementById("btn7"),
+};
+
+const url = "https://pokeapi.co/api/v2/";
+
+async function getData(endpoint) {
+  try {
+    const response = await fetch(url + endpoint);
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {}
 }
 
-
-const url = 'https://pokeapi.co/api/v2/';
-
-async function getData(endpoint){
- try {
-	const response = await fetch(url + endpoint);
-	const result = await response.json();
-	console.log(result);
-} catch (error) {
-	
-}}
-
 async function getPokeInfo() {
-	const ditto = await getData(`type/ground`);
-	console.log('Pokemon Info', ditto);
+  const ditto = await getData(`type/ground`);
+  console.log("Pokemon Info", ditto);
 }
 
 getPokeInfo();
 
-function createGallery(){
-	DOMSelectors.gallery.innerHTML = "";
-
-
+function createGallery(arr) {
+  DOMSelectors.gallery.innerHTML = "";
+  arr.forEach((data) => {
+    DOMSelectors.gallery.insertAdjacentElement(
+      "beforeend",
+      `div class="card">
+<h2 class="name">${data.name}`
+    );
+  });
 }
 
-
-
-
-
 DOMSelectors.themeSwitch.addEventListener("click", () => {
-	const currentTheme = document.body.classList.contains("cool")
-	  ? "warm"
-	  : "cool";
-	document.body.classList.toggle("cool");
-	document.body.classList.toggle("warm");
-	setTheme(currentTheme);
-  });
+  const currentTheme = document.body.classList.contains("cool")
+    ? "warm"
+    : "cool";
+  document.body.classList.toggle("cool");
+  document.body.classList.toggle("warm");
+  setTheme(currentTheme);
+});
