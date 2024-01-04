e4740d1df88dde970ada6cafafb34aa03868a9e5
eriks61 <eriks61@nycstudents.net>
2024-01-04 09:24:24 -0500

    df

--- api-project/js/dom.js
+++ api-project/js/dom.js
@@ -7,6 +7,8 @@ const DOMSelectors = {
   ghost: document.getElementById("btn6"),
   gallery: document.getElementById("card-container"),
   themeSwitch: document.getElementById("btn7"),
+  h2: document.getElementById("h2"),
+  search: document.querySelector("input[name='Find a berry']")
 };
 
 const url = "https://pokeapi.co/api/v2/pokemon/";
@@ -24,7 +26,7 @@ async function getData(url){
              `<div class="card">
              <h2 class ="name">${data.name}</h2>
              <h3 class ="type">${data.type}</h3>
-             `
+             </div>`
              
              
              );
@@ -32,9 +34,54 @@ async function getData(url){
              }
             )   
             };
+            createGallery(data)
+            data.forEach((data) => {console.log(data.name)})
         }
-    }
+        catch(error){
+          DOMSelectors.h2.textContent="Can't find!"
+        }
+    DOMSelectors.enter.addEventListener("submit", function(event){
+      event.preventDefault();
+      clear();
+      let value = DOMSelectors.search.value
+      let link = `https://pokeapi.co/api/v2/berry/${value}`
+      async function getData(link){
+        try{
+          const response = await fetch (link);
+          if (response.status !=200){
+            throw new Error (response.statusText);
+          }
+          const data = await response.json();
+          function createGallery(arr){
+            arr.forEach((data) => {
+              DOMSelectors.gallery.insertAdjacentHTML(
+                'beforeend', 
+                `<div class="card>
+                <h2 class="name">${data.name}</h2>
+                <h2 class="size">${data.size}</h2>
+                <h2 class="smoothness">${data.smoothness}</h2>`
+              )
+            })
+          };
+          createGallery(data);
+        }
+        catch (error) {
+          console.log("no info");
+          document.querySelector("h2").textContent = "Berry doesn't exist!"
+        DOMSelectors.form.addEventListener
+        
+        }
+      }
+    })
+      }
+      getData(url)
 
+      function clear(){
+        DOMSelectors.gallery.innerHTML = "";
+      }
+      function clearhead(){
+        DOMSelectors.h2.innerHTML=""
+      }
 async function getPokeInfo() {
   const ditto = await getData(`ground`);
   console.log("Pokemon Info", ditto);
